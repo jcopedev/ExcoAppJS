90745506802830b47b093c566352c083c9d68f13
Darcy Eason <d_eason@fanshaweonline.ca>
2020-10-24 19:40:45 -0400

    'delete' functionality

--- excoapp/app.js
+++ excoapp/app.js
@@ -7,27 +7,30 @@ var db = firebase.firestore();
 const inputTextField = document.querySelector("#mainInput");
 const saveButton = document.querySelector("#save");
 
-saveButton.addEventListener("click", function(){
-	
-	const textToSave = inputTextField.value;
-    db.collection("posts").add({
-    username: "Anonymous",
-	message: textToSave,
-	credits: 1,
-	shieldPoints: 0,
-	isPaid: false,
-	dateCreated: 0,
-	dateDeleted: 0,
-	isHidden: false,
-   })
-   .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-		alert("Congratulations! You shared your thoughts with the world. We filed your message in very special location: at the very bottom of the website!");
-		window.location.href = 'limitedtimeline.html'; //relative to domain
-   })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-   });
-  
-   console.log("saving exco-message");
+saveButton.addEventListener("click", function () {
+  const textToSave = inputTextField.value;
+  db.collection("posts")
+    .add({
+      username: "Anonymous",
+      message: textToSave,
+      credits: 1,
+      deletes: 0,
+      shieldPoints: 0,
+      isPaid: false,
+      dateCreated: 0,
+      dateDeleted: 0,
+      isHidden: false,
+    })
+    .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      alert(
+        "Congratulations! You shared your thoughts with the world. We filed your message in very special location: at the very bottom of the website!"
+      );
+      window.location.href = "limitedtimeline.html"; //relative to domain
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+    });
+
+  console.log("saving exco-message");
 });
--- excoapp/fullTimeline.js
+++ excoapp/fullTimeline.js
@@ -4,28 +4,46 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
-db.collection("posts").get().then((querySnapshot) => {
+//if signed in
+db.collection("posts")
+  .get()
+  .then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
+      if (
+        doc.data().credits > doc.data().deletes ||
+        doc.data().deletes == null
+      ) {
         console.log(`${doc.data().username} => ${doc.data().message}`);
-		var userName = document.createElement("H2");   
-        userName.innerHTML = `${doc.data().username}`;                  
-        document.body.appendChild(userName);               
-		
-		var messageContent = document.createElement("H4");   
-        messageContent.innerHTML = `${doc.data().message}`;                   
-        document.body.appendChild(messageContent);              
-				
-	    var creditCount = document.createElement("p");   
-        creditCount.innerHTML = `ExcoCredits: ${doc.data().credits}`;                  
-        document.body.appendChild(creditCount);               
-				
-		var deleteBtn = document.createElement("BUTTON");   
-        deleteBtn.innerHTML = "Delete This!";                  
-        document.body.appendChild(deleteBtn);               
-		
-        deleteBtn.onclick = function(){
-        alert('Warning: You are about to delete this message from the entire website. It will be the last time anyone ever sees it. Are you sure you want to do that? Also, you will probably make the person who wrote this message sad.');return false;
-        };
-    });
-});
+        var userName = document.createElement("H2");
+        userName.innerHTML = `${doc.data().username}`;
+        document.body.appendChild(userName);
 
+        var messageContent = document.createElement("H4");
+        messageContent.innerHTML = `${doc.data().message}`;
+        document.body.appendChild(messageContent);
+
+        var creditCount = document.createElement("p");
+        creditCount.innerHTML = `ExcoCredits: ${doc.data().credits}`;
+        document.body.appendChild(creditCount);
+
+        var deleteBtn = document.createElement("BUTTON");
+        deleteBtn.innerHTML = "Delete This!";
+        document.body.appendChild(deleteBtn);
+
+        deleteBtn.onclick = function () {
+          alert(
+            "Warning: You are about to delete this message from the entire website. It will be the last time anyone ever sees it. Are you sure you want to do that? Also, you will probably make the person who wrote this message sad."
+          );
+          var delCount = doc.data().deletes;
+          if (delCount == null) {
+            doc.ref.update({ deletes: 1 });
+          } else {
+            doc.ref.update({
+              deletes: firebase.firestore.FieldValue.increment(1),
+            });
+          }
+          return false;
+        };
+      }
+    });
+  });
