598af5301f0416d344f26ee9371c3067bc2668c0
Sandeep <sandeep75492@gmail.com>
2020-10-25 01:46:17 -0400

    Frontend for Signup/in page, User profile added.

--- excoapp/userProfile.js
+++ excoapp/userProfile.js
@@ -1,25 +1,23 @@
 //Initialize Firebase
 if (!firebase.apps.length) {
-   firebase.initializeApp({});
+  firebase.initializeApp({});
 }
 
 var user = firebase.auth().currentUser;
 var name, email, photoUrl, uid, emailVerified;
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
-      name = user.displayName;
-      email = user.email;
-      photoUrl = user.photoURL;
-      emailVerified = user.emailVerified;
-      uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                   // this value to authenticate with your backend server, if
-                   // you have one. Use User.getToken() instead.
-	  alert(email);
-	  //alert(photoUrl);
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
+    // this value to authenticate with your backend server, if
+    // you have one. Use User.getToken() instead.
+    alert(email);
+    //alert(photoUrl);
   } else {
     alert("not signed in");
   }
 });
-
-
