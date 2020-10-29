fbc5928e4939337d579b3c55007e23fe639e3729
Sandeep <sandeep75492@gmail.com>
2020-10-29 00:46:36 -0400

    Frontend for Create Post nd Timeline (in progress)

--- excoapp/app.js
+++ excoapp/app.js
@@ -25,7 +25,8 @@ saveButton.addEventListener("click", function () {
       alert(
         "Congratulations! You shared your thoughts with the world. We filed your message in very special location: at the very bottom of the website!"
       );
-      window.location.href = "limitedtimeline.html"; //relative to domain
+      window.location.href = "Usertimeline.html"; //relative to domain
+      //can be change to "LimitedTimeline.html" for testing
     })
     .catch(function (error) {
       console.error("Error adding document: ", error);
@@ -34,53 +35,53 @@ saveButton.addEventListener("click", function () {
   console.log("saving exco-message");
 });
 
-var el;                                                    
+var el;
 
-function countCharacters(e) {                                    
-  var textEntered, countRemaining, counter;          
-  textEntered = document.getElementById('mainInput').value;  
-  counter = (500 - (textEntered.length));
-  countRemaining = document.getElementById('charactersRemaining'); 
-  countRemaining.textContent = counter + " characters remaining...";       
+function countCharacters(e) {
+  var textEntered, countRemaining, counter;
+  textEntered = document.getElementById("mainInput").value;
+  counter = 500 - textEntered.length;
+  countRemaining = document.getElementById("charactersRemaining");
+  countRemaining.textContent = counter + " characters remaining...";
 }
-el = document.getElementById('mainInput');                   
-el.addEventListener('keyup', countCharacters, false);
+el = document.getElementById("mainInput");
+el.addEventListener("keyup", countCharacters, false);
 
-//This works but it's annoying when you are working so I commented it out. 
+//This works but it's annoying when you are working so I commented it out.
 // function GetCookie(name) {
-  // var arg=name+"=";
-  // var alen=arg.length;
-  // var clen=document.cookie.length;
-  // var i=0;
-  // while (i<clen) {
-    // var j=i+alen;
-    // if (document.cookie.substring(i,j)==arg)
-      // return "here";
-    // i=document.cookie.indexOf(" ",i)+1;
-    // if (i==0) break;
-  // }
-  // return null;
+// var arg=name+"=";
+// var alen=arg.length;
+// var clen=document.cookie.length;
+// var i=0;
+// while (i<clen) {
+// var j=i+alen;
+// if (document.cookie.substring(i,j)==arg)
+// return "here";
+// i=document.cookie.indexOf(" ",i)+1;
+// if (i==0) break;
+// }
+// return null;
 // }
 // function manageVisitor(){
-    // //do something if brand new user
-    // var visit=GetCookie("FirstTimeVisitCookie");
-    // if (visit==null){
-       // var expire=new Date();
-       // expire=new Date(expire.getTime()+7776000000);
-       // document.cookie="FirstTimeVisitCookie=here; expires="+expire + ";path=/";
-   // } else {
-       // firebase.auth().onAuthStateChanged(function(user) {
-            // if (user) {
-                // //Do something if user is already logged in
-                // window.location.href = 'userProfile.html'; // Returning visitor goes to timeline
-            // } else {
-                // //Do something if user is returning, but not logged in
-                // window.location.href = 'limitedtimeline.html'; // Returning visitor goes to timeline (limitetimeline.html in excoappjs)
-            // }
-        // });
+// //do something if brand new user
+// var visit=GetCookie("FirstTimeVisitCookie");
+// if (visit==null){
+// var expire=new Date();
+// expire=new Date(expire.getTime()+7776000000);
+// document.cookie="FirstTimeVisitCookie=here; expires="+expire + ";path=/";
+// } else {
+// firebase.auth().onAuthStateChanged(function(user) {
+// if (user) {
+// //Do something if user is already logged in
+// window.location.href = 'userProfile.html'; // Returning visitor goes to timeline
+// } else {
+// //Do something if user is returning, but not logged in
+// window.location.href = 'limitedtimeline.html'; // Returning visitor goes to timeline (limitetimeline.html in excoappjs)
+// }
+// });
 
-   // }
+// }
 // }
 // $(document).ready(function(){
-    // manageVisitor();
+// manageVisitor();
 // });
--- excoapp/fullTimeline.js
+++ excoapp/fullTimeline.js
@@ -9,6 +9,20 @@ db.collection("posts")
   .orderBy("credits", "desc")
   .get()
   .then((querySnapshot) => {
+    document.body.style.backgroundImage =
+      "linear-gradient( 135deg, #92FFC0 10%, #002661 100%)";
+
+    //Overall Timeline Div
+    var timelineDiv = document.createElement("div");
+    timelineDiv.id = "timelineDiv";
+    timelineDiv.className = "timelineDiv";
+    document.body.appendChild(timelineDiv);
+
+    timelineDiv.style.textAlign = "center";
+    timelineDiv.style.width = "80%";
+    timelineDiv.style.paddingLeft = "20%";
+
+    var count = 1;
     querySnapshot.forEach((doc) => {
       if (doc.data().credits > 0) {
         console.log(
@@ -16,21 +30,58 @@ db.collection("posts")
             doc.data().dateCreated
           }`
         );
-        var userName = document.createElement("H2");
-        userName.innerHTML = `${doc.data().username}`;
-        document.body.appendChild(userName);
 
-        var messageContent = document.createElement("H4");
+        //post Div
+        var postDiv = document.createElement("div");
+        postDiv.id = "postDiv" + count;
+        postDiv.className = "postDiv";
+        document.getElementById("timelineDiv").appendChild(postDiv);
+
+        var userName = document.createElement("h4");
+        userName.innerHTML = `${doc.data().username}`;
+        document.getElementById("postDiv" + count).appendChild(userName);
+        userName.style.textAlign = "left";
+        userName.style.padding = "2.5%";
+
+        var messageContent = document.createElement("p");
         messageContent.innerHTML = `${doc.data().message}`;
-        document.body.appendChild(messageContent);
+        document.getElementById("postDiv" + count).appendChild(messageContent);
+        messageContent.style.textAlign = "left";
+        messageContent.style.padding = "2.5%";
 
         var creditCount = document.createElement("p");
         creditCount.innerHTML = `ExcoCredits: ${doc.data().credits}`;
-        document.body.appendChild(creditCount);
+        document.getElementById("postDiv" + count).appendChild(creditCount);
+        creditCount.style.textAlign = "right";
+        creditCount.style.padding = "2.5%";
 
         var deleteBtn = document.createElement("BUTTON");
         deleteBtn.innerHTML = "Delete This!";
-        document.body.appendChild(deleteBtn);
+        document.getElementById("postDiv" + count).appendChild(deleteBtn);
+        deleteBtn.style.textAlign = "right";
+        deleteBtn.style.display = "inline-block";
+        deleteBtn.style.padding = "0.35em 1.2em";
+        deleteBtn.style.border = "0.1em solid #FFFFFF";
+        deleteBtn.style.margin = "0 0.3em 0.3em 0";
+        deleteBtn.style.borderRadius = "0.12em";
+        deleteBtn.style.boxSizing = "border-box";
+        deleteBtn.style.fontFamily = "'Roboto',sans-serif";
+        deleteBtn.style.fontWeight = "300";
+        deleteBtn.style.color = "#FFFFFF";
+        deleteBtn.style.transition = "all 0.2s";
+        deleteBtn.style.backgroundColor = "#343A40";
+
+        var breakElement = document.createElement("br");
+        document.getElementById("postDiv" + count).appendChild(breakElement);
+
+        document.getElementById("postDiv" + count).style.border =
+          "5px solid #0BB5B3	";
+        document.getElementById("postDiv" + count).style.boxShadow =
+          "20px 20px rgba(0,0,0,.15)";
+        document.getElementById("postDiv" + count).style.borderRadius = "25px";
+        document.getElementById("postDiv" + count).style.marginBottom = "25px";
+        document.getElementById("postDiv" + count).style.marginTop = "15px";
+        count++;
 
         deleteBtn.onclick = function () {
           alert(
