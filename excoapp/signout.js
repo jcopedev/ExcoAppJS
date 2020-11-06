3503a573bb56a6ab0762366416df809e8893d55f
Aaron VanDuynhoven <AaronVAudio@gmail.com>
2020-11-06 16:16:28 -0500

    navigation tweaks and incomplete sign out

---
+++ excoapp/signout.js
@@ -0,0 +1,13 @@
+//Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp({});
+  }
+
+function signout() {
+    firebase.auth().signOut().then(function() {
+        alert("Success!");
+        window.location.href='/limitedtimeline.html';
+      }).catch(function(error) {
+        alert("Error!");
+      });
+};
