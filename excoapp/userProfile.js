c19370c1256ddfc08bd8947b6f4a25d78a74ea82
jamesfan <j_cope@fanshaweonline.ca>
2020-11-07 19:35:47 -0500

    placeholder profile info

--- excoapp/userProfile.js
+++ excoapp/userProfile.js
@@ -15,9 +15,15 @@ firebase.auth().onAuthStateChanged(function (user) {
     uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
     // this value to authenticate with your backend server, if
     // you have one. Use User.getToken() instead.
-    alert(name);
+    //alert(photoURL);
     //alert(photoUrl);
   } else {
     alert("not signed in");
   }
 });
+
+
+//this isn't working and I'm not sure why
+document.getElementById("usernameContainer").innerHTML = name;
+document.getElementById("emailContainer").innerHTML = email;
+document.getElementById("picContainer").innerHTML = photoURL;
