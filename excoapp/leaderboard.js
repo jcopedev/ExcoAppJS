0ea6212a1ea87b635b614b1a046dc2e4446f4b0c
Sandeep <sandeep75492@gmail.com>
2020-11-19 18:21:04 -0500

    Leaderboard, Delete, advertiser front-end

--- excoapp/leaderboard.js
+++ excoapp/leaderboard.js
@@ -8,47 +8,53 @@ var user = firebase.auth().currentUser;
 
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
-    var signin = document.getElementById('signin');
+    var signin = document.getElementById("signin");
     signin.style.display = "none";
   } else {
-    var signout = document.getElementById('signout');
+    var signout = document.getElementById("signout");
     signout.style.display = "none";
   }
 });
 
 function makeCard(postId, userName, message, excoCredits) {
-	htmlString = `<div class = "divBreak" id = ${postId}>
-					<div class="card" style="width: 28rem;">
+  htmlString = `<div class = "divBreak" id = ${postId} >
+					<div class="card" style="width: 35rem; background-color: white; font-size: 35px; border-radius: 25px;
+					border: 2px solid #73AD21;
+					padding: 20px;
+					" >
 					<div class="card-body">
-					<h5 class="card-title">${userName}</h5>
+					<h5 class="card-title" style="font-weight: bold; font-size: 30px;">${userName}</h5>
 				    <h6 class="card-subtitle mb-2 text-muted">Survival Time (sec.): ${excoCredits}</h6>
 					<p class="card-text">${message}</p>
 				</div>
 					</div>
 					</br>
 				</div>
-				`
-    return htmlString;
+				`;
+  return htmlString;
 }
 
+function loadFirstPage() {
+  var bottomPost;
+  db.collection("posts")
+    .where("credits", "==", 0)
+    .orderBy("timeAlive", "desc")
+    .limit(50)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        postId = doc.id;
+        userName = doc.data().username.trim();
+        message = doc.data().message.trim();
+        timeAlive = doc.data().timeAlive;
+        bottomPost = doc.data().dateCreated.toDate();
 
-function loadFirstPage(){
-		var bottomPost;
-		db.collection("posts")
-				.where("credits", "==", 0)
-				.orderBy("timeAlive", "desc")
-				.limit(50)
-				.get()
-				.then((querySnapshot) => {
-				querySnapshot.forEach((doc) => {
-				
-					postId = doc.id;
-					userName = doc.data().username.trim();
-					message = doc.data().message.trim();
-					timeAlive = doc.data().timeAlive;
-					bottomPost = doc.data().dateCreated.toDate();
-						
-					cardContainer.innerHTML += makeCard(postId, userName, message, timeAlive);						
-			});
-		  }) 	
-	}
+        cardContainer.innerHTML += makeCard(
+          postId,
+          userName,
+          message,
+          timeAlive
+        );
+      });
+    });
+}
--- excoapp/userProfile.js
+++ excoapp/userProfile.js
@@ -1,7 +1,7 @@
 //Initialize Firebase
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
-  }
+}
 
 var user = firebase.auth().currentUser;
 var name, email, photoUrl, uid, emailVerified;
@@ -16,7 +16,7 @@ firebase.auth().onAuthStateChanged(function (user) {
     // this value to authenticate with your backend server, if
     // you have one. Use User.getToken() instead.
     //alert(name);
-		
+
     //this wasn't working outside this scope not sure why
     document.getElementById("emailContainer").innerHTML += email;
     document.getElementById("picContainer").innerHTML += photoURL;
@@ -30,3 +30,42 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 document.getElementById("usernameContainer").innerHTML += name;
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+function signout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      alert("Success!");
+      window.location.href = "./limitedtimeline.html";
+    })
+    .catch(function (error) {
+      alert("Error!");
+      window.location.href = "./userProfile.html";
+    });
+}
